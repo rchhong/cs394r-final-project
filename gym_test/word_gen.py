09998f52411578536a0e81c8252223ba8720d031
thebunanna <thebunanna@gmail.com>
2022-04-20 23:57:09 -0500

    added new word list and word gen script

--- gym_test/word_gen.py
+++ gym_test/word_gen.py
@@ -1,3 +1,19 @@
+import sys
+import random
 
 
-# if "__main__":
+if "__main__":
+    if (len(sys.argv) != 3):
+        print ("No input file? or number count really")
+    try:
+        int (sys.argv[2])
+    except:
+        print ("Not a number")
+        sys.exit(1)
+
+    with open ("output.txt", "w+") as output:
+        with open(sys.argv[1], "r+") as f:
+            lines = f.readlines()
+            output.writelines(random.sample(lines, int(sys.argv[2])))
+            
+
